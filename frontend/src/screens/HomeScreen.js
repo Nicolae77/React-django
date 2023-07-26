04db91f8250405497204c119182b940af0945f15
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-07-26 13:04:32 +0100

    HomeScreen. js file created

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Row, Col } from 'react-boostrap'
+
+import products from '../products'
+
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Latest Products</h1>
+    </div>
+  )
+}
