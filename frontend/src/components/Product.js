6a009b8e164bc8c8104725e8bc6f6d38daec0ddd
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-08-15 12:50:41 +0100

    Rating components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-
+import Rating from './Rating'
 
 export default function Product({product}) {
   return (
 
         <Card className="my-3 p-3 rounded">
-            <a href={'/product/${product._id}'}>
+            <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image} />
             </a>
 
             <Card.Body>
-            <a href={'/product/${product._id}'}>
+            <a href={`/product/${product._id}`}>
                 <Card.Title as="div">
                     <strong>{product.name}</strong>
                 </Card.Title>
@@ -19,12 +19,12 @@ export default function Product({product}) {
 
             <Card.Text as='div'>
                 <div className='my-3'>
-                    {product.rating} from {product.numReviews} reviews
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
                 </div>
             </Card.Text>
 
             <Card.Text as='h3'>
-                ${[product.price]}
+                ${product.price}
             </Card.Text>
 
             </Card.Body>
